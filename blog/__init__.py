db986e9583ddd2af0784b0452215f8f85bdf6fa6
yuriikhomych <yuriykhomich@gmail.com>
2020-03-07 19:20:02 +0200

    Create blog blueprint

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 
 from blog.config import Configuration
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Configuration)
 
+    db.init_app(app)
+
     from blog.main.view import main
+    from blog.posts.view import posts
 
     app.register_blueprint(main)
+    app.register_blueprint(posts, url_prefix='/blog')
 
     return app
--- blog/config.py
+++ blog/config.py
@@ -1,2 +1,4 @@
 class Configuration:
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/flask_blog'
