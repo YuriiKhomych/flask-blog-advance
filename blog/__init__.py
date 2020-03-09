8b956697cd0b2c6ed0b9d22f4b00119aed38b709
yuriikhomych <yuriykhomich@gmail.com>
2020-03-09 20:50:29 +0200

    Add Flask-Admin to blog

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-from blog.config import Configuration
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
+
+from blog.config import Configuration
 
 db = SQLAlchemy()
 
@@ -17,5 +20,12 @@ def create_app():
 
     app.register_blueprint(main)
     app.register_blueprint(posts, url_prefix='/blog')
+
+    ### ADMIN ###
+    from blog.posts.models import Post, Tag
+
+    admin = Admin(app)
+    admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Tag, db.session))
 
     return app
--- blog/config.py
+++ blog/config.py
@@ -2,3 +2,4 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/flask_blog'
+    SECRET_KEY = 'very secret key'
